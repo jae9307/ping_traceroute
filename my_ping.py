81fe7c5a1b0ce9875cfce174991b77712f828b3b
JoseEstevez <jae9307@g.rit.edu>
2025-02-19 21:02:20 -0500

    implemented -s flag in code; checksum changed

--- my_ping.py
+++ my_ping.py
@@ -2,38 +2,34 @@ import struct
 import socket
 import argparse
 import time
+from multiprocessing import Process
 
 
-def create_packet(seq_num):
+def create_packet(seq_num, payload_size):
     type = 8
     code = 0
     checksum_placeholder = 0
     identifier = 23  # randomly chosen number
     seq_number = seq_num
+    payload = bytes(payload_size)
+    if len(payload) % 2 == 1:
+        payload += b'\x00'  # if payload is odd length of bytes, pad so it can be used for checksum
 
     initial_packet = struct.pack('!BBHHH', type, code, checksum_placeholder, identifier, seq_number)
+    packet_with_payload = initial_packet + payload
 
-    first_pair_bytes = initial_packet[:2]
-    second_pair_bytes = initial_packet[2:4]
-    third_pair_bytes = initial_packet[4:6]
-    fourth_pair_bytes = initial_packet[6:]
+    checksum = 0
+    for index in range(0, len(packet_with_payload), 2):
+        checksum += (packet_with_payload[index] << 8) + packet_with_payload[index + 1]
+    checksum = (checksum >> 16) + (checksum & 0xFFFF)
+    checksum = ~checksum & 0xFFFF
 
-    calculated_checksum = bin(int.from_bytes(first_pair_bytes, byteorder='big', signed=False)
-                           + int.from_bytes(second_pair_bytes, byteorder='big', signed=False)
-                           + int.from_bytes(third_pair_bytes, byteorder='big', signed=False)
-                           + int.from_bytes(fourth_pair_bytes, byteorder='big', signed=False))
-
-    complemented_checksum = (~int(calculated_checksum, 2) & 0xFFFF)
-    # complemented_checksum = bin(0xFFFF - int(calculated_checksum, 2))
-
-    return struct.pack('!BBHHH', type, code, complemented_checksum, identifier, seq_number)
+    return packet_with_payload[:2] + struct.pack('!H', checksum) + packet_with_payload[4:]
 
 def send_packet(packet, address):
     raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
     try:
         raw_socket.sendto(packet, (address, 1))
-    except PermissionError:
-        print("You need admin")
     finally:
         raw_socket.close()
 
@@ -44,20 +40,12 @@ def recieve_packet():
     print(f"Received packet from {addr}: {packet}")
     raw_socket.close()
 
-def main():
-    # Define command line parameters.
-    parser = argparse.ArgumentParser(prog='my_ping', description='Sends and receives ICMP echo packets')
-    parser.add_argument('address')
-    parser.add_argument('-c', action='store')
-    parser.add_argument('-i', action='store')
-    parser.add_argument('-s', action='store_true')
-    parser.add_argument('-t', action='store_true')
-
-    args = parser.parse_args()
+def ping(args):
+    payload_size = int(args.s) if args.s is not None else 56
 
     iteration = 0
     while True:
-        packet = create_packet(iteration)
+        packet = create_packet(iteration, payload_size)
 
         send_packet(packet, args.address)
 
@@ -69,6 +57,23 @@ def main():
 
         time_to_sleep = 1 if args.i is None else int(args.i)
         time.sleep(time_to_sleep)
+
+def main():
+    # Define command line parameters.
+    parser = argparse.ArgumentParser(prog='my_ping', description='Sends and receives ICMP echo packets')
+    parser.add_argument('address')
+    parser.add_argument('-c', action='store')
+    parser.add_argument('-i', action='store')
+    parser.add_argument('-s', action='store')
+    parser.add_argument('-t', action='store')
+
+    args = parser.parse_args()
+
+    ping_process = Process(target=ping(args))
+    ping_process.start()
+    if args.t is not None:
+        ping_process.join(timeout=int(args.t))
+        ping_process.terminate()
 
 if __name__ == '__main__':
     main()
