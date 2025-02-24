103137c740f05ae72f6052333e13f8569b3efd01
JoseEstevez <jae9307@g.rit.edu>
2025-02-24 00:41:47 -0500

    added documentation

--- my_ping.py
+++ my_ping.py
@@ -1,3 +1,4 @@
+"""Implement ping functionality"""
 import struct
 import socket
 import argparse
@@ -7,6 +8,13 @@ import numpy as np
 
 
 def create_packet(seq_num, payload_size):
+    """
+    create an ICMP packet for ping
+
+    :param seq_num: the sequence number of the packet
+    :param payload_size: size of the packet's payload
+    :return: the ICMP packet
+    """
     type = 8
     code = 0
     checksum_placeholder = 0
@@ -14,28 +22,48 @@ def create_packet(seq_num, payload_size):
     seq_number = seq_num
     payload = bytes(payload_size)
     if len(payload) % 2 == 1:
-        payload += b'\x00'  # if payload is odd length of bytes, pad so it can be used for checksum
+        payload += b'\x00'  # if payload is odd length of bytes, pad
+        # so it can be used for checksum
 
-    initial_packet = struct.pack('!BBHHH', type, code, checksum_placeholder, identifier, seq_number)
+    initial_packet = struct.pack('!BBHHH', type, code, checksum_placeholder,
+                                 identifier, seq_number)
     packet_with_payload = initial_packet + payload
 
+    # Calculate the packet's checksum
     checksum = 0
     for index in range(0, len(packet_with_payload), 2):
-        checksum += (packet_with_payload[index] << 8) + packet_with_payload[index + 1]
+        checksum += ((packet_with_payload[index] << 8)
+                     + packet_with_payload[index + 1])
     checksum = (checksum >> 16) + (checksum & 0xFFFF)
     checksum = ~checksum & 0xFFFF
 
-    return packet_with_payload[:2] + struct.pack('!H', checksum) + packet_with_payload[4:]
+    return (packet_with_payload[:2] + struct.pack('!H', checksum)
+            + packet_with_payload[4:])
 
 def send_packet(packet, address):
-    raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    """
+    send a packet using a raw socket
+
+    :param packet: the packet to send
+    :param address: IP address to send the packet to
+    """
+    raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW,
+                               socket.IPPROTO_ICMP)
     try:
         raw_socket.sendto(packet, (address, 1))
     finally:
         raw_socket.close()
 
 def recieve_packet(start_time):
-    raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    """
+    attempt to receive the reply to the ping ICMP echo request
+
+    :param start_time: time the request packet was sent
+    :return: 1 or 0 depending on if the reply was received or not,
+    followed by the round trip time of the packet
+    """
+    raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW,
+                               socket.IPPROTO_ICMP)
     raw_socket.bind(('0.0.0.0', 0))
     raw_socket.settimeout(3)
 
@@ -56,6 +84,11 @@ def recieve_packet(start_time):
     return 1, rtt
 
 def ping(args):
+    """
+    send echo requests and measure the number of replies and trip times
+
+    :param args: command line arguments
+    """
     payload_size = int(args.s) if args.s is not None else 56
     print(f"Pinging {args.address} with {payload_size} bytes")
 
@@ -75,6 +108,11 @@ def ping(args):
         round_trip_times.append(rtt)
 
         iteration += 1
+
+        # If -c command line parameter is used, stop ping program after
+        # sending and attempting to receive the specified number of
+        # packets, and calculate and display statistics regarding the
+        # number of packets received or lost and the round trip times
         if args.c is not None and iteration >= int(args.c):
             round_trip_times.sort()
             sum = 0
@@ -82,12 +120,16 @@ def ping(args):
                 sum += trip_time
             average_rtt = sum/len(round_trip_times)
 
+            lost_pkts = num_pkts_sent - num_pkts_received
+
             print(f"Ping statistics for {args.address}:")
-            print(f"    Packets: Sent = {num_pkts_sent}, Received = {num_pkts_received}, "
-                  f"Lost = {num_pkts_sent - num_pkts_received} "
-                  f"({((num_pkts_sent - num_pkts_received)/num_pkts_sent) * 100}% loss),")
+            print(f"    Packets: Sent = {num_pkts_sent}, "
+                  f"Received = {num_pkts_received}, "
+                  f"Lost = {lost_pkts} "
+                  f"({(lost_pkts/num_pkts_sent) * 100}% loss),")
             print("Approximate round trip times in milli-seconds:")
-            print(f"    Minimum = {round_trip_times[0]}ms, Maximum = {round_trip_times[-1]}ms, "
+            print(f"    Minimum = {round_trip_times[0]}ms, "
+                  f"Maximum = {round_trip_times[-1]}ms, "
                   f"Average = {average_rtt}ms")
             break
 
@@ -95,8 +137,10 @@ def ping(args):
         time.sleep(time_to_sleep)
 
 def main():
+    """parse arguments, call ping(), and terminate ping() after timeout"""
     # Define command line parameters.
-    parser = argparse.ArgumentParser(prog='my_ping', description='Sends and receives ICMP echo packets')
+    parser = argparse.ArgumentParser(
+        prog='my_ping', description='Sends and receives ICMP echo packets')
     parser.add_argument('address')
     parser.add_argument('-c', action='store')
     parser.add_argument('-i', action='store')
--- my_traceroute.py
+++ my_traceroute.py
@@ -1,3 +1,4 @@
+"""Implement traceroute functionality"""
 import struct
 import socket
 import argparse
@@ -5,37 +6,64 @@ import time
 import numpy as np
 
 def get_checksum(data):
+    """
+    calculate the checksum for a packet
+
+    :param data: packet bytes
+    :return: the checksum
+    """
     checksum = 0
     for index in range(0, len(data), 2):
         checksum += (data[index] << 8) + data[index + 1]
     checksum = (checksum >> 16) + (checksum & 0xFFFF)
     return ~checksum & 0xFFFF
 
-def create_packet(seq_num, iteration):
+def create_packet(seq_num):
+    """
+    create an ICMP packet for traceroute
+
+    :param seq_num: the sequence number of the packet
+    :return: the ICMP packet
+    """
     type = 8
     code = 0
     checksum_placeholder = 0
     identifier = 23  # randomly chosen number
     seq_number = seq_num
 
-    initial_packet = struct.pack('!BBHHH', type, code, checksum_placeholder, identifier, seq_number)
-
+    initial_packet = struct.pack('!BBHHH', type, code, checksum_placeholder,
+                                 identifier, seq_number)
     icmp_checksum = get_checksum(initial_packet)
-
-    icmp_packet = initial_packet[:2] + struct.pack('!H', icmp_checksum) + initial_packet[4:]
+    icmp_packet = (initial_packet[:2] + struct.pack('!H', icmp_checksum)
+                   + initial_packet[4:])
 
     return icmp_packet
 
 def send_packet(packet, address, ttl):
-    raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    """
+    send a packet using a raw socket
+
+    :param packet: the packet to send
+    :param address: IP address to send packet to
+    :param ttl: maximum # of network hops the packet can take
+    """
+    raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW,
+                               socket.IPPROTO_ICMP)
     raw_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
     try:
         raw_socket.sendto(packet, (address, 1))
     finally:
         raw_socket.close()
 
-def recieve_packet(start_time, probe):
-    raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+def recieve_packet(start_time):
+    """
+    attempt to receive the reply to the traceroute ICMP echo request
+
+    :param start_time: time the request packet was sent
+    :return: source address of reply packet
+    """
+    raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW,
+                               socket.IPPROTO_ICMP)
     raw_socket.bind(('0.0.0.0', 0))
     raw_socket.settimeout(3)
 
@@ -56,6 +84,11 @@ def recieve_packet(start_time, probe):
     return address
 
 def trace_route(args):
+    """
+    incrementally send probes to discover a route to a given IP address
+
+    :param args: command line arguments
+    """
     num_probes = int(args.q) if args.q is not None else 3
 
     iteration = 1
@@ -67,18 +100,22 @@ def trace_route(args):
 
         print(iteration, end ="    ")
 
+        # Send multiple probes and receive their replies
         address = 'Request Timed Out'
         for probe in range(num_probes):
-            packet = create_packet(seq_num, iteration)
+            packet = create_packet(seq_num)
             start_time = time.time()
             send_packet(packet, args.address, iteration)
-            seq_num += 1
-            reply_address = recieve_packet(start_time, probe)
+            reply_address = recieve_packet(start_time)
             if reply_address is not None:
                 address = reply_address
             else:
                 num_unanswered_probes += 1
 
+            seq_num += 1
+
+        # Print hostname and IP address of source of reply packet, or
+        # only the IP address if -n flag is included
         if args.n is False:
             try:
                 hostname = socket.gethostbyaddr(address)
@@ -97,8 +134,11 @@ def trace_route(args):
         print(f'Number of unanswered probes: {num_unanswered_probes}')
 
 def main():
+    """Parse command line arguments and call trace_route() function"""
     # Define command line parameters.
-    parser = argparse.ArgumentParser(prog='my_traceroute', description='Sends and receives ICMP echo packets')
+    parser = argparse.ArgumentParser(
+        prog='my_traceroute',
+        description='Sends and receives ICMP echo packets')
     parser.add_argument('address')
     parser.add_argument('-n', action='store_true')
     parser.add_argument('-q', action='store')
